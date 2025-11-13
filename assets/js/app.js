0ed2721a4ebac8822e961015a6a4e5d062c0417c
Armin  Bayu <arminbayu07@gmail.com>
2025-11-13 11:35:02 +0700

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,21 @@
-// Set tahun otomatis di footer
-document.addEventListener("DOMContentLoaded", function () {
-    var yearEl = document.getElementById("year");
-    if (yearEl) {
-        yearEl.textContent = new Date().getFullYear();
-    }
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('a[href^="#"]');
+    const offset = 0; // misal pakai navbar fixed tinggi 80px -> isi 80
+
+    links.forEach(link => {
+        link.addEventListener("click", e => {
+            const targetId = link.getAttribute("href").slice(1);
+            const target = document.getElementById(targetId);
+            if (!target) return;
+
+            e.preventDefault();
+
+            const targetPosition = target.getBoundingClientRect().top + window.scrollY - offset;
+
+            window.scrollTo({
+                top: targetPosition,
+                behavior: "smooth"
+            });
+        });
+    });
 });

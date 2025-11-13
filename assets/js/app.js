83ba4e08c4e3b7f94455f78cab235ff1790cfdde
Armin  Bayu <arminbayu07@gmail.com>
2025-11-13 09:00:37 +0700

    update

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// Set tahun otomatis di footer
+document.addEventListener("DOMContentLoaded", function () {
+    var yearEl = document.getElementById("year");
+    if (yearEl) {
+        yearEl.textContent = new Date().getFullYear();
+    }
+});
